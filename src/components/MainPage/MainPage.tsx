a8b56d3387233d4095bdd59e43b4474c3ece93e5
Michał Furgała <mchfrg@gmail.com>
2023-07-08 13:12:25 +0200

    Add Colors and Projects Section

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -8,13 +8,22 @@ const MainPage: React.FC = () => {
 		<div className='main-page'>
 			<div className='content'>
 				<div className='leftContent'>
-					<h1>Hey, I am Michal</h1>
-					<h2>Welcome to my Website</h2>
+					<h1>Michał Furgała</h1>
+					<h2>Witaj na mojej stronie</h2>
 					<button className='contact-button' onClick={handleContactClick}>
 						Contact
 					</button>
 				</div>
-				<div className='rightContent'></div>
+				<div className='rightContent'>
+					<h1>About me</h1>
+					<div className='rightTextContent'><p>
+						Jestem studentem informatyki na <a>Politechnice Wrocławskiej</a>, gdzie zdobywam wiedzę z zakresu
+						programowania i informatyki. Przed maturą uczestniczyłem w bootcampie JavaScript. W czasie przygotowań do
+						matury poznałem podstawy języka Python, a na pierwszym roku studiów poznałem podstawy języków programowania
+						C/C++ oraz Java. Jestem bardzo zmotywowany, aby rozwijać swoje umiejętności programistyczne i poznać nowe
+						technologie.
+					</p></div>
+				</div>
 			</div>
 			<div className='scroll-down-arrow' onClick={handleArrowClick}>
 				<span className='arrow'>&#8595;</span>
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HamburgerMenu from './components/NavBar/HamburgerMenu';
 import MainPage from './components/MainPage/MainPage';
+import Projects from './components/Projects/Projects';
 const App: React.FC = () => {
 	return (
 		<div id='root' style={{ background: '323232' }}>
 			<HamburgerMenu />
 			<MainPage></MainPage>
-			<div style={{ height: '100vh', margin: '0', background: '#C0C0C0' }}></div>
-			<div style={{ height: '100vh', margin: '0', background: '#212121' }}></div>
-			<div style={{ height: '100vh', margin: '0', background: '#C0C0C0' }}></div>
+
+			<Projects></Projects>
 		</div>
 	);
 };
