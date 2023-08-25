b5e5c67cefc5673e377d1f7c7114bb5d5888787f
Michał Furgała <mchfrg@gmail.com>
2023-08-25 22:31:18 +0200

    addNetlifyAndTailwindCssToskilLCarouselle

--- src/components/SkillsComponent/Skills.tsx
+++ src/components/SkillsComponent/Skills.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { FaHtml5, FaCss3Alt, FaNode, FaReact } from 'react-icons/fa';
 // SiMongodb
-import { SiTypescript, SiSass, SiMysql, SiJavascript, SiGithub } from 'react-icons/si';
+import { SiTypescript, SiSass, SiMysql, SiJavascript, SiGithub, SiNetlify, SiTailwindcss } from 'react-icons/si';
 
 import './Skills.css';
 
@@ -17,6 +17,8 @@ const SkillsCarousel: React.FC = () => {
 		// { icon: <SiMongodb className='skill-icon' />, name: 'MongoDB' },
 		{ icon: <SiMysql className='skill-icon' />, name: 'MySQL' },
 		{ icon: <SiGithub className='skill-icon' />, name: 'Git' },
+		{ icon: <SiNetlify className='skill-icon' />, name: 'Netlify' },
+		{ icon: <SiTailwindcss className='skill-icon' />, name: 'Tailwindcss' },
 	];
 	const [currentSlide, setCurrentSlide] = useState(0);
 
